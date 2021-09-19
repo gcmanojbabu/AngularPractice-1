a1ca599b37397ad9731938508351f020500ed272
gmanojbabu148 <gmanojbabu1999@gmail.com>
2021-09-19 20:59:03 +0530

    feat:AssignDisplayDetailsComponent

--- src/app/assign-display-details/assign-display-details.component.ts
+++ src/app/assign-display-details/assign-display-details.component.ts
@@ -15,6 +15,7 @@ export class AssignDisplayDetailsComponent implements OnInit {
 
   onToggleDetails() {
     this.showPass = !this.showPass;
-    this.log.push(this.log.length + 1);
+    // this.log.push(this.log.length + 1);
+    this.log.push(new Date());
   }
 }
